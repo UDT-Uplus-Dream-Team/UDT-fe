2e4f67752fd5b9f23423f6b2addf8bbe8be47d3e
Junho Heo <hanascreen@konkuk.ac.kr>
2025-07-08 22:39:04 +0900

    [UDT-44] feat: 포스터 이미지와 제목을 표시하는 카드 컴포넌트 제작

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,6 +8,13 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+      backgrounds: {
+        default: 'dark',
+        values: [
+          { name: 'dark', value: '#121212' },
+          { name: 'light', value: '#ffffff' }, // 필요 시 다른 테마도 추가 가능
+        ],
+      },
     },
   },
 };

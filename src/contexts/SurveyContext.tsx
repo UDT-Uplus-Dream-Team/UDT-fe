2a108781fe8e5939a57bb24209bcfaeb5e82ca2d
qowldk <qowldk1030@gmail.com>
2025-07-11 09:26:01 +0900

    [UDT-49] refactor: useSurveyContext 훅을 별도 파일로 분리

--- src/contexts/SurveyContext.tsx
+++ src/contexts/SurveyContext.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { SurveyContextType } from '@type/SurveyContext';
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useState } from 'react';
 
-const SurveyContext = createContext<SurveyContextType | null>(null);
+export const SurveyContext = createContext<SurveyContextType | null>(null);
 
 export function SurveyProvider({ children }: { children: React.ReactNode }) {
   const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
@@ -24,12 +24,4 @@ export function SurveyProvider({ children }: { children: React.ReactNode }) {
       {children}
     </SurveyContext.Provider>
   );
-}
-
-export function useSurveyContext() {
-  const context = useContext(SurveyContext);
-  if (!context) {
-    throw new Error('useSurveyContext must be used within a SurveyProvider');
-  }
-  return context;
 }
---
+++ src/hooks/useSurveyContext.ts
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { SurveyContext } from '@/contexts/SurveyContext';
+
+export function useSurveyContext() {
+  const context = useContext(SurveyContext);
+  if (!context) {
+    throw new Error(
+      'useSurveyContext 훅은 SurveyProvider 내부에서만 사용해야 합니다.',
+    );
+  }
+  return context;
+}

d1dd775ad1679c5a9cfa48e3434a053446b4d067
choi-hi <gmltmdsp@hanmail.net>
2025-07-15 16:07:45 +0900

    [UDT-81] refactor: 추천 삭제 api 수정

--- src/hooks/profile/useDeleteFeedback.ts
+++ src/hooks/profile/useDeleteFeedback.ts
@@ -1,14 +1,16 @@
+import { deleteFeedback } from '@/lib/apis/profile/feedbackService';
+import { FeedbackQueryParams } from '@/types/mypage/RecommendedContent';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { deleteFeedback } from '@lib/apis/profile/feedbackService';
 
-export const useDeleteFeedback = () => {
+export const useDeleteFeedback = (
+  baseParams: Omit<FeedbackQueryParams, 'cursor'>,
+) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (feedbackId: number) => deleteFeedback(feedbackId),
     onSuccess: () => {
-      // 삭제 성공 시 피드백 콘텐츠 리스트 다시 불러오기
-      queryClient.invalidateQueries({ queryKey: ['feedbackContents'] });
+      queryClient.invalidateQueries({ queryKey: ['feedbacks', baseParams] });
     },
   });
 };

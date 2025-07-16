dab9fe9ce938b7e487f18978d3aa2f2605fef2cc
qowldk <qowldk1030@gmail.com>
2025-07-16 15:46:38 +0900

    [UDT-90] feat: 콘텐츠 수정 API 및 훅 구현

---
+++ src/hooks/admin/usePatchContent.ts
@@ -0,0 +1,21 @@
+import { patchContent } from '@lib/apis/admin/patchContent';
+import { ContentWithoutId } from '@type/admin/Content';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+// 콘텐츠 수정을 처리하는 훅
+export const useUpdateContent = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      contentId,
+      data,
+    }: {
+      contentId: number;
+      data: ContentWithoutId;
+    }) => patchContent(contentId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['adminContentList'] });
+    },
+  });
+};

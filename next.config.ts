32881cc764a60d665e2fc812db291c3fb65186b1
Junho Heo <hanascreen@konkuk.ac.kr>
2025-07-25 15:37:42 +0900

    hotfix: next-image 컴포넌트에서 이미지 최적화를 오류 해결을 위해 next.config.ts 추가 수정

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
+// next.config.ts
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['s3-udt-dev.s3.ap-northeast-2.amazonaws.com'],
-  },
-};
-
-module.exports = {
-  images: {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 's3-udt-dev.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**', // S3의 모든 경로 허용
+      },
+      {
+        protocol: 'https',
         hostname: 'k.kakaocdn.net',
+        pathname: '/**', // 카카오 CDN 이미지 경로 허용
       },
     ],
   },

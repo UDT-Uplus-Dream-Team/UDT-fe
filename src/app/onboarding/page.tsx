75c904c41921528f04d020b930f20dca5cb1b45d
choi-hi <gmltmdsp@hanmail.net>
2025-08-01 10:35:32 +0900

    [UDT-208] refactor: fallback 멘트 한글로 변경

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -3,7 +3,9 @@ import OnboardingClient from './OnboardingClient';
 
 export default function OnboardingPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={<div>데이터를 불러오고 있어요, 잠시만 기다려주세요.</div>}
+    >
       <OnboardingClient />
     </Suspense>
   );

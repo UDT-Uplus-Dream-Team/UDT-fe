eee94ac0f30b4978e996ec2f39afe1df2958cb94
qowldk <qowldk1030@gmail.com>
2025-08-04 01:43:45 +0900

    [UDT-225] feat: 출연진 검색, 출연진 등록 기능 추가

---
+++ src/hooks/admin/useGetCastsSearch.ts
@@ -0,0 +1,19 @@
+import { getCastsSearch } from '@lib/apis/admin/getCastsSearch';
+import { AdminCastsGetRequest } from '@type/admin/Cast';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+export const useInfiniteCastsSearch = (
+  initialParams: Omit<AdminCastsGetRequest, 'cursor'>,
+) => {
+  return useInfiniteQuery({
+    queryKey: ['infiniteAdminCasts', initialParams],
+    queryFn: ({ pageParam = '' }) =>
+      getCastsSearch({ ...initialParams, cursor: pageParam }).then(
+        (res) => res.data,
+      ),
+    getNextPageParam: (lastPage) =>
+      lastPage.hasNext ? lastPage.nextCursor : undefined,
+    initialPageParam: '',
+    enabled: !!initialParams.name,
+  });
+};
--- src/hooks/admin/usePostUploadImages.ts
+++ src/hooks/admin/usePostUploadImages.ts
@@ -1,21 +1,8 @@
 import { useMutation } from '@tanstack/react-query';
 import { postUploadImages } from '@lib/apis/admin/postUploadImages';
-import { showSimpleToast } from '@components/common/Toast';
 
 export const usePostUploadImages = () => {
   return useMutation({
     mutationFn: postUploadImages,
-    onSuccess: () => {
-      showSimpleToast.success({
-        message: '이미지가 성공적으로 업로드되었습니다.',
-        position: 'top-center',
-      });
-    },
-    onError: () => {
-      showSimpleToast.error({
-        message: '이미지 업로드에 실패했습니다.',
-        position: 'top-center',
-      });
-    },
   });
 };

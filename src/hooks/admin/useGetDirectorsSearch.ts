236b94bd0d2556757454f4c526c06eb6901a8aff
qowldk <qowldk1030@gmail.com>
2025-08-04 02:34:52 +0900

    [UDT-225] feat: 감독 검색, 감독 등록 api 함수와 훅 구현

---
+++ src/hooks/admin/useGetDirectorsSearch.ts
@@ -0,0 +1,19 @@
+import { getDirectorsSearch } from '@lib/apis/admin/getDirectorsSearch';
+import { AdminDirectorsGetRequest } from '@type/admin/Director';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+export const useInfiniteDirectorsSearch = (
+  initialParams: Omit<AdminDirectorsGetRequest, 'cursor'>,
+) => {
+  return useInfiniteQuery({
+    queryKey: ['infiniteAdminDirectors', initialParams],
+    queryFn: ({ pageParam = '' }) =>
+      getDirectorsSearch({ ...initialParams, cursor: pageParam }).then(
+        (res) => res.data,
+      ),
+    getNextPageParam: (lastPage) =>
+      lastPage.hasNext ? lastPage.nextCursor : undefined,
+    initialPageParam: '',
+    enabled: !!initialParams.name,
+  });
+};

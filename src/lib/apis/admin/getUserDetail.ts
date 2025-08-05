a3a8045a2ab3141282e26c1da971209739078b6b
choi-hi <gmltmdsp@hanmail.net>
2025-08-05 10:43:47 +0900

    [UDT-209] feature: 유저 정보 관리 목록  무한스크롤 api 제작

--- src/lib/apis/admin/getUserDetail.ts
+++ src/lib/apis/admin/getUserDetail.ts
@@ -2,6 +2,6 @@ import { UserDetail } from '@type/admin/user';
 import axiosInstance from '../axiosInstance';
 
 export const getUserDetail = async (userId: number): Promise<UserDetail> => {
-  const res = await axiosInstance.get(`/api/admin/users/${userId}`);
+  const res = await axiosInstance.get(`/api/admin/users/${userId}/metrics`);
   return res.data;
 };
--- src/types/admin/user.ts
+++ src/types/admin/user.ts
@@ -11,7 +11,7 @@ export interface User {
 }
 
 export interface CursorPageResponse<T> {
-  items: T[];
+  item: T[];
   hasNext: boolean;
   nextCursor: number | null;
 }

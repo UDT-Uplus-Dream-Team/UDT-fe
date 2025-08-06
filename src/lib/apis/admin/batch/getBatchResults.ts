8e7c004ec9f4c3848af2f2c5e218a83de8cab6ad
Junho Heo <hanascreen@konkuk.ac.kr>
2025-08-06 17:32:42 +0900

    [UDT-211] feat: getBatchResults.ts response 값에 대해 type 안정성 강화

--- src/lib/apis/admin/batch/getBatchResults.ts
+++ src/lib/apis/admin/batch/getBatchResults.ts
@@ -1,6 +1,12 @@
 import axiosInstance from '@lib/apis/axiosInstance';
 import { BatchResult } from '@type/admin/batch';
 
+interface BatchResultsApiResponse {
+  item: BatchResult[];
+  hasNext: boolean;
+  nextCursor: string | null;
+}
+
 // 배치 결과 목록 조회 API
 export const getBatchResults = async ({
   cursor,
@@ -12,12 +18,15 @@ export const getBatchResults = async ({
   results: BatchResult[];
   nextCursor: string | null; // 다음 페이지 커서 (null이면 끝)
 }> => {
-  const { data } = await axiosInstance.get('/api/admin/batch/results', {
-    params: {
-      cursor,
-      size,
+  const { data } = await axiosInstance.get<BatchResultsApiResponse>(
+    '/api/admin/batch/results',
+    {
+      params: {
+        cursor,
+        size,
+      },
     },
-  });
+  );
   return {
     results: data.item,
     nextCursor: data.hasNext ? data.nextCursor : null, // hasNext가 true면 nextCursor 반환, false면 null 반환
